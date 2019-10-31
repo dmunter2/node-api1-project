f75645a7c6b9c8648135dc209378a11f3c83df1f
dmunter2 <49967133+dmunter2@users.noreply.github.com>
2019-10-31 11:54:46 -0700

    added delete put and get

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ server.get('/api/users', (req, res) => {
 })
 
 
-
+// create a new one
 server.post('/api/users', (req, res) => {
     const userInfo = req.body;
     db.insert(userInfo);
@@ -49,16 +49,36 @@ server.post('/api/users', (req, res) => {
 })
 
 
-server.put('/api/users/:id', (req, res) => {
-    const userInfo = req.body
-    const id = req.body.id
 
-    db.update(userInfo, id)
+// delete one thats already on file//
+server.delete('/api/users/:id', (req, res) => {
+    const {id} = req.params;
+    db.remove(id)
+      .then(user => {
+          if (user){
+              res.status(200).json({success: true}).end()
+          } else {
+              res.status(404).json({success: false})
+          }
+      })
+      .catch(err => {
+          res.status(500).json({success: false, err})
+      })
+})
+
+
+
+// edit one that we have on file
+server.put('/api/users/:id', (req, res) => {
+    const id = req.body.id
+    const userInfo = req.body
+
+    db.update(id, userInfo)
         .then(user => {
             if (user) {
                 res.status(200).json({success: true, user});
             } else {
-                res.status(404).json({success: false, message: `id ${id} does not exist`})
+                res.status(404).json({success: false, message: `id  does not exist`})
             }
         })
         .catch(err => {
@@ -90,72 +110,4 @@ server.put('/api/users/:id', (req, res) => {
 
 
 
-
-
-
-
-// // const http = require('http');
-// // const hostname = '127.0.0.1';
-// // const port = 3000;
-
-// const express = require('express');
-// const db = require('./data/db')
-// const server = express();
-// const port = 4000;
-
-// server.listen(4000, () => {
-//     console.log('=== server listening')
-// });
-
-
-
-// // const server = http.createServer((req, res) => {
-// //     res.statusCode = 200; 
-// //     res.setHeader('Content-Type', 'text/plain');
-// //     res.end('Hello World from Node\n');
-// // });
-
-// server.listen(port, hostname, () => {
-//     console.log(`server is running at http://${hostname}:${port}/`)
-// });
-
-
-
-// // first action is to read 
-
-// server.get('/api/users', (req, res) => {
-//     db.find()
-//         .then((users) => {
-//             res.status(200).json(users);
-//         })
-//         .catch((err) => {
-//             res.status(500).json({
-//                 message: err,
-//                 success: false
-//             });
-//         });
-// })
-
-
-// // post request from client 
-
-// server.post('/api/users', (req, res) => {
-//     const user = req.body 
-//     console.log(userInfo)
-//     db.insert(userInfo)
-//         .then(user => {
-//             if( !user.name || !user.bio) {
-//                 res.status(400).json({errorMessage: 'Please provide bio and user' })
-//             } else {
-//                 res.status(201).json(user)
-//             }
-//         })
-//         .catch(err => {
-//             res.status(500).json({error: 'There was an error'})
-//         })
-
-
-
-
-// })
 
